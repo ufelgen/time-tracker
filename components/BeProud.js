a2d4e068fe730ff5697346a628595eda12b09cf8
ufelgen <felgenhauer.u@gmail.com>
2023-07-09 14:17:55 +0200

    position box

--- components/BeProud.js
+++ components/BeProud.js
@@ -13,7 +13,7 @@ const StyledDiv = styled.div`
   height: auto;
   width: 80vw;
   top: 30vh;
-  margin-left: 10vw !important;
+  margin: 0 auto !important;
 
   background-color: lightgoldenrodyellow;
   color: var(--primary);
