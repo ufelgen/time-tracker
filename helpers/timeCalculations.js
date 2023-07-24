03543aed3e25d0298426571fa8ebe30b8715a905
ufelgen <felgenhauer.u@gmail.com>
2023-07-24 21:40:34 +0200

    fix time difference function

--- helpers/timeCalculations.js
+++ helpers/timeCalculations.js
@@ -6,27 +6,10 @@ export function determineTimeDifference(startTime, stopTime) {
   const stopTimeHours = stopTime.split(":")[0];
   const stopTimeMinutes = stopTime.split(":")[1];
 
-  if (
-    (stopTimeHours > startTimeHours && stopTimeMinutes > startTimeMinutes) ||
-    (stopTimeHours > startTimeHours && stopTimeMinutes === startTimeMinutes)
-  ) {
-    const timeDifference =
-      (stopTimeHours - startTimeHours) * 60 +
-      (stopTimeMinutes - startTimeMinutes);
-    return timeDifference;
-  } else if (stopTimeHours === startTimeHours) {
-    const timeDifference = stopTimeMinutes - startTimeMinutes;
-    return timeDifference;
-  } else if (
-    stopTimeHours > startTimeHours &&
-    stopTimeMinutes < startTimeMinutes
-  ) {
-    const timeDifference =
-      (stopTimeHours - startTimeHours) * 60 -
-      60 +
-      (startTimeMinutes - stopTimeMinutes);
-    return timeDifference;
-  }
+  const timeDifference =
+    (stopTimeHours - startTimeHours) * 60 +
+    (stopTimeMinutes - startTimeMinutes);
+  return timeDifference;
 }
 
 export function storeStart(
